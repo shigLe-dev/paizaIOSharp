c2b7a1efc5e751ecdd1baeed886d8687a1ae3769
KYapp <kukiyomenaisapp@gmail.com>
2022-12-21 12:32:57 +0900

    言語テーブル使ってみた

--- Test/Program.cs
+++ Test/Program.cs
@@ -6,6 +6,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine(PaizaIO.Run("print(\"aaaaa\")", "python3").stdOut);
+        Console.WriteLine(PaizaIO.Run("print(\"aaaaa\")", Language.Python3).stdOut);
     }
 }
