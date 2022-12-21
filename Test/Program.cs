868ce836c2943d8420c99362c171894f59a5da63
KYapp <kukiyomenaisapp@gmail.com>
2022-12-21 12:38:49 +0900

    RunAsyncの方も追加

--- Test/Program.cs
+++ Test/Program.cs
@@ -7,5 +7,6 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine(PaizaIO.Run("print(\"aaaaa\")", Language.Python3).stdOut);
+        Console.WriteLine(PaizaIO.RunAsync("print(\"bbbbb\")", Language.Python3).Result.stdOut);
     }
 }
