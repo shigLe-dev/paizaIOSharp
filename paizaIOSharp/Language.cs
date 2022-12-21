259d5423d21cc52fe726f248921d3b60584f7cb2
KYapp <kukiyomenaisapp@gmail.com>
2022-12-21 12:31:04 +0900

    言語テーブルを追加

---
+++ paizaIOSharp/Language.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace paizaIOSharp;
+
+public static class Language
+{
+    public const string C = "c";
+    public const string Cpp = "cpp";
+    public const string ObjectiveC = "objective-c";
+    public const string Java = "java";
+    public const string Kotlin = "kotlin";
+    public const string Scala = "scala";
+    public const string Swift = "swift";
+    public const string CSharp = "csharp";
+    public const string Go = "go";
+    public const string Haskell = "haskell";
+    public const string Erlang = "erlang";
+    public const string Perl = "perl";
+    public const string Python2 = "python";
+    public const string Python3 = "python3";
+    public const string Ruby = "ruby";
+    public const string PHP = "php";
+    public const string Bash = "bash";
+    public const string R = "r";
+    public const string JavaScript = "javascript";
+    public const string CoffeeScript = "coffeescript";
+    public const string VisualBasic = "vb";
+    public const string Cobol = "cobol";
+    public const string FSharp = "fsharp";
+    public const string D = "d";
+    public const string Clojure = "clojure";
+    public const string Elixir = "elixir";
+    public const string MySQL = "mysql";
+    public const string Rust = "rust";
+    public const string Scheme = "scheme";
+    public const string CommonLisp = "commonlisp";
+    public const string Nadesiko = "nadesiko";
+    public const string TypeScript = "typescript";
+    public const string Plain = "plain";
+}
